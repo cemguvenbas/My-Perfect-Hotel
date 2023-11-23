312cf2ca044f8d52fecc9e1e16cdaa0939434d64
Cem Güvenbaş <cemguvenbass@gmail.com>
2023-11-23 18:47:05 +0300

    Cleaning System

--- MyPerfectHotel/Assets/My Perfect Hotel - Cleaning System v2/Assets/MPH - Cleaning System/Scripts/Cleanable.cs
+++ MyPerfectHotel/Assets/My Perfect Hotel - Cleaning System v2/Assets/MPH - Cleaning System/Scripts/Cleanable.cs
@@ -5,69 +5,19 @@ using UnityEngine.UI;
 
 public class Cleanable : MonoBehaviour
 {
-    [Header(" Elements ")]
-    [SerializeField] private Image fillImage;
-    [SerializeField] private Animator animator;
-    [SerializeField] private Canvas canvas;
-    [SerializeField] private ParticleSystem cleanParticles;
+    [Header("Elements")]
+    [SerializeField] private Image gageFill;
 
-    [Header(" Settings ")]
-    private bool isClean;
-
-    // Start is called before the first frame update
-    void Start()
+    public void Clean(float value)
     {
-        MessUp();
-    }
+        gageFill.fillAmount += value;
 
-    // Update is called once per frame
-    void Update()
-    {
-        FaceCanvas();
-    }
-
-    private void FaceCanvas()
-    {
-        canvas.transform.forward = (Camera.main.transform.position - canvas.transform.position).normalized;
-    }
-
-    public void CleanProcess(float value)
-    {
-        fillImage.fillAmount += value;
-
-        if (fillImage.fillAmount >= 1)
+        if (gageFill.fillAmount >= 1)
             SetAsClean();
     }
 
     private void SetAsClean()
     {
-        isClean = true;
-
-        canvas.gameObject.SetActive(false);
-
-        animator.Play("Clean");
-
-        Debug.Log(string.Format("{0} is now clean", name));
-
-        cleanParticles.Play();
+        Debug.Log("Hey, I'm clean");
     }
-
-    public void MessUp()
-    {
-        isClean = false;
-
-        canvas.gameObject.SetActive(true);
-
-        fillImage.fillAmount = 0;
-
-        animator.Play("MessUp");
-
-        Debug.Log("Messing up " + name);
-    }
-
-    public bool IsClean()
-    {
-        return isClean;
-    }
-
 }
--- MyPerfectHotel/Assets/My Perfect Hotel - Cleaning System v2/Assets/MPH - Cleaning System/Scripts/CleaningSystem.cs
+++ MyPerfectHotel/Assets/My Perfect Hotel - Cleaning System v2/Assets/MPH - Cleaning System/Scripts/CleaningSystem.cs
@@ -2,46 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(PlayerController))]
 public class CleaningSystem : MonoBehaviour
 {
-    [Header(" Elements ")]
-    private PlayerController playerController;
-
-    [Header(" Settings ")]
+    [Header("Settings")]
     [SerializeField] private float detectionRadius;
-    [SerializeField] private float cleanSpeed;
-
-    [Header(" Test ")]
-    [SerializeField] private int frameRate;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        playerController = GetComponent<PlayerController>();
-        Application.targetFrameRate = frameRate;
-    }
-
-    // Update is called once per frame
+    [SerializeField] private float cleaningSpeed;
+    
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-            Debug.Log("Mouse");
-
-        if (Input.GetKeyDown(KeyCode.Space))
-            Debug.Log("Space");
-
-        if (Input.GetKeyDown(KeyCode.M))
-        {
-            Debug.Log("Here");
-            
-            foreach (Cleanable cleanable in FindObjectsOfType<Cleanable>())
-                cleanable.MessUp();
-
-        }
-
-        if (!playerController.IsMoving())
-            DetectCleanables();
+        DetectCleanables();
     }
 
     private void DetectCleanables()
@@ -49,13 +18,13 @@ public class CleaningSystem : MonoBehaviour
         Collider[] detectedColliders = Physics.OverlapSphere(transform.position, detectionRadius);
 
         foreach (Collider collider in detectedColliders)
-            if (collider.transform.parent.TryGetComponent(out Cleanable cleanable))
-            {
-                if (!cleanable.IsClean())
-                {
-                    cleanable.CleanProcess(cleanSpeed * Time.deltaTime);
-                    break;
-                }
-            }
+            if (collider.TryGetComponent(out Cleanable cleanable))
+                cleanable.Clean(cleaningSpeed * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, detectionRadius);
     }
 }
